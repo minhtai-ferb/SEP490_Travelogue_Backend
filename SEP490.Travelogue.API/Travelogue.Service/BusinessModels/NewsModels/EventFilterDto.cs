47011f1e935c8577230b783bb76ae7da2e2cb618
hungnt-ds <hungnt.dfs@gmail.com>
2025-08-06 15:21:39 +0700

    feat: add endpoints for paginated event, news, and experience retrieval with filtering options

---
+++ SEP490.Travelogue.API/Travelogue.Service/BusinessModels/NewsModels/EventFilterDto.cs
@@ -0,0 +1,8 @@
+namespace Travelogue.Service.BusinessModels.NewsModels;
+
+public class EventFilterDto
+{
+    public string? Title { get; set; }
+    public int? Month { get; set; }
+    public int? Year { get; set; }
+}
