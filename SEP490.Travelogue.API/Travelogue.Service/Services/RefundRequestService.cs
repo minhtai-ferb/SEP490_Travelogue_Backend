0d0f908b2b93565ad9ad7b321842ef9357233506
hungnt-ds <hungnt.dfs@gmail.com>
2025-08-12 22:13:55 +0700

    feat: Update booking status, add service to send refund request

--- SEP490.Travelogue.API/Travelogue.Service/Services/RefundRequestService.cs
+++ SEP490.Travelogue.API/Travelogue.Service/Services/RefundRequestService.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Travelogue.Repository.Bases.Exceptions;
 using Travelogue.Repository.Data;
+using Travelogue.Repository.Entities;
 using Travelogue.Repository.Entities.Enums;
 using Travelogue.Service.BusinessModels.RefundRequestModels;
 using Travelogue.Service.Commons.Interfaces;
@@ -15,48 +16,85 @@ public class RefundRequestService : IRefundRequestService
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IUserContextService _userContextService;
+    private readonly ITimeService _timeService;
 
-    public RefundRequestService(IUnitOfWork unitOfWork, IUserContextService userContextService)
+    public RefundRequestService(IUnitOfWork unitOfWork, IUserContextService userContextService, ITimeService timeService)
     {
         _unitOfWork = unitOfWork;
         _userContextService = userContextService;
+        _timeService = timeService;
     }
 
-    // public async Task CreateRefundRequestAsync(RefundRequestCreateDto dto)
-    // {
-    //     var userId = Guid.Parse(_userContextService.GetCurrentUserId());
+    public async Task CreateRefundRequestAsync(RefundRequestCreateDto dto)
+    {
+        using var transaction = await _unitOfWork.BeginTransactionAsync();
+        try
+        {
+            var currentUserId = Guid.Parse(_userContextService.GetCurrentUserId());
+            var currentTime = _timeService.SystemTimeNow;
 
-    //     var booking = await _unitOfWork.BookingRepository
-    //         .ActiveEntities
-    //         .FirstOrDefaultAsync(b => b.Id == dto.BookingId && b.UserId == userId)
-    //         ?? throw CustomExceptionFactory.CreateNotFoundError("Booking");
+            var booking = await _unitOfWork.BookingRepository
+                .ActiveEntities
+                .FirstOrDefaultAsync(b => b.Id == dto.BookingId && b.UserId == currentUserId)
+                ?? throw CustomExceptionFactory.CreateNotFoundError("Booking");
 
-    //     // Rule: Không cho tạo request nếu đã có pending hoặc approved
-    //     var existingRequest = await _unitOfWork.RefundRequestRepository
-    //         .ActiveEntities
-    //         .FirstOrDefaultAsync(r => r.BookingId == dto.BookingId &&
-    //             (r.Status == RefundRequestStatus.Pending || r.Status == RefundRequestStatus.Approved));
+            // Rule: Không cho tạo request nếu đã có pending hoặc approved
+            var existingRequest = await _unitOfWork.RefundRequestRepository
+                .ActiveEntities
+                .FirstOrDefaultAsync(r => r.BookingId == dto.BookingId &&
+                    (r.Status == RefundRequestStatus.Pending || r.Status == RefundRequestStatus.Approved));
 
-    //     if (existingRequest != null)
-    //         throw CustomExceptionFactory.CreateBadRequestError("Đã tồn tại yêu cầu hoàn tiền cho booking này.");
+            if (existingRequest != null)
+                throw CustomExceptionFactory.CreateBadRequestError("Đã tồn tại yêu cầu hoàn tiền cho booking này.");
 
-    //     if (booking.Status != BookingStatus.Confirmed && booking.Status != BookingStatus.CancelledByProvider)
-    //         throw CustomExceptionFactory.CreateBadRequestError("Trạng thái booking không cho phép hoàn tiền.");
+            if (!(booking.Status == BookingStatus.Confirmed
+                || booking.Status == BookingStatus.CancelledByProvider))
+            {
+                throw CustomExceptionFactory.CreateBadRequestError("Booking không đủ điều kiện hoàn tiền.");
+            }
 
-    //     if (dto.RefundAmount > booking.FinalPrice)
-    //         throw CustomExceptionFactory.CreateBadRequestError("Số tiền hoàn vượt quá số tiền đã thanh toán.");
+            // thời gian hoàn tiền trước 48 tiếng
+            if (booking.Status == BookingStatus.Confirmed)
+            {
+                var hoursBefore = (booking.StartDate - DateTime.UtcNow).TotalHours;
+                if (hoursBefore < 48)
+                    throw CustomExceptionFactory.CreateBadRequestError("Không thể hoàn tiền do hủy quá sát giờ.");
+            }
 
-    //     var refundRequest = new RefundRequest
-    //     {
-    //         Id = Guid.NewGuid(),
-    //         UserId = userId,
-    //         BookingId = dto.BookingId,
-    //         RefundAmount = dto.RefundAmount,
-    //         Status = RefundRequestStatus.Pending
-    //     };
+            if (dto.RefundAmount > booking.FinalPrice)
+                throw CustomExceptionFactory.CreateBadRequestError("Số tiền hoàn vượt quá số tiền đã thanh toán.");
 
-    //     await _unitOfWork.RefundRequestRepository.AddAsync(refundRequest);
-    //     await _unitOfWork.SaveAsync();
-    // }
+            var existing = await _unitOfWork.RefundRequestRepository
+                .ActiveEntities
+                .AnyAsync(r => r.BookingId == dto.BookingId &&
+                    (r.Status == RefundRequestStatus.Pending || r.Status == RefundRequestStatus.Approved));
+
+            if (existing)
+                throw CustomExceptionFactory.CreateBadRequestError("Đã tồn tại yêu cầu hoàn tiền cho booking này.");
+
+            var refundRequest = new RefundRequest
+            {
+                Id = Guid.NewGuid(),
+                UserId = currentUserId,
+                BookingId = dto.BookingId,
+                RefundAmount = dto.RefundAmount,
+                Status = RefundRequestStatus.Pending
+            };
+
+            await _unitOfWork.RefundRequestRepository.AddAsync(refundRequest);
+            await _unitOfWork.SaveAsync();
+            await transaction.CommitAsync();
+        }
+        catch (CustomException)
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+            throw CustomExceptionFactory.CreateInternalServerError(ex.Message);
+        }
+    }
 
 }
